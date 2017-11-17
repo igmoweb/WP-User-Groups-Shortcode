1d9eb7b68064be2d99a8857ef82023be8c9d0d53
Ignacio Cruz <igmoweb@gmail.com>
2017-11-17 14:52:29 +0100

    Add user link

--- user-template.php
+++ user-template.php
@@ -4,13 +4,20 @@
 		wp_kses_allowed_html( 'user_description' )
 	);
 	$description = wpautop( $description );
+
+	$link = sprintf( '<a href="%1$s" title="%2$s" rel="author">%3$s</a>',
+		esc_url( get_author_posts_url( $user->ID, $user->user_nicename ) ),
+		/* translators: %s: author's display name */
+		esc_attr( sprintf( __( 'Posts by %s' ), $user->display_name ) ),
+		$user->display_name
+	);
 ?>
 <li>
 	<div class="user-avatar">
 		<?php echo get_avatar( $user->ID, 96 ); ?>
 	</div>
 	<h3 class="user-name">
-		<?php echo $user->display_name; ?>
+		<?php echo $link; ?>
 	</h3>
 	<div class="user-bio">
 		<?php echo $description ?>
